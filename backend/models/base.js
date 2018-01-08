8ec9889e0dad665add16f40737c212a0928322bf
Jon Ablondi <jonnyablondi@hotmail.com>
2018-01-08 18:44:08 +1100

    creating mock data in database to ensure connections are correct and different models are speaking to each other

--- backend/models/base.js
+++ backend/models/base.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = Promise
 const db = mongoose.connection
 
-// db.on('open', () => { console.log('Successful connection to MongoDB')});
-mongoose.connect('mongodb://localhost/profiles', { useMongoClient: true })
+db.on('open', () => { console.log('Successful connection to MongoDB')});
+mongoose.connect('mongodb://localhost/finalProfiles', { useMongoClient: true })
 
 module.exports = mongoose
--- backend/models/profile.js
+++ backend/models/profile.js
@@ -1,26 +1,18 @@
-// const mongoose = require('./base')
-// const Schema = mongoose.Schema
-// const ObjectId = Schema.Types.ObjectId
-//
-// const ProfileSchema = Schema({
-//   email: String,
-//   password: String,
-//   factory_name: String,
-//   address: String,
-//   hkid: String,
-//   incorporation_certificate: String,
-//   payment_method: String,
-//   invoices: [{ type: ObjectId, ref: 'Invoice' }]
-// })
-//
-// const Profile = mongoose.model('Final_Proj_Profile', ProfileSchema)
+const mongoose = require('./base')
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
 
-const Profile = [
-  {
-    factory_name: 'Jo Ablo',
-    address: '123 Fake St',
-    hkid: 'N-1191938'
-  }
-]
+const FinalProfileSchema = Schema({
+  email: String,
+  password: String,
+  factory_name: String,
+  address: String,
+  hkid: String,
+  incorporation_certificate: String,
+  payment_method: String,
+  invoices: [{ type: ObjectId, ref: 'Invoice' }]
+})
+
+const Profile = mongoose.model('finalProfile', FinalProfileSchema)
 
 module.exports = Profile
