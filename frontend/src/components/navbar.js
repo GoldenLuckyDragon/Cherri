afd1a701d748c3bac81cd46fde3081fa57134851
Carmen <carmenhchung@gmail.com>
2018-01-08 19:31:11 +1100

    edit navbar to include right aligned burger with changes to css

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,42 +1,61 @@
 import React from 'react'
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
+
+// import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
+//
+// export default class Navigation extends React.Component {
+//   constructor (props) {
+//     super(props)
+//
+//     this.toggleNavbar = this.toggleNavbar.bind(this)
+//     this.state = {
+//       collapsed: true
+//     }
+//   }
+//
+//   toggleNavbar () {
+//     this.setState({
+//       collapsed: !this.state.collapsed
+//     })
+//   }
+//   render () {
+//     return (
+//       <div>
+//         <Navbar color='faded' light>
+//           <NavbarBrand href='/' className='mr-auto'>Logo</NavbarBrand>
+//           <NavbarToggler onClick={this.toggleNavbar} className='mr-2' />
+//           <Collapse isOpen={!this.state.collapsed} navbar>
+//             <Nav navbar>
+//               <NavItem>
+//                 <NavLink href='/home'>Home</NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink href='/dashboard'>Dashboard</NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink href='/account'>Account</NavLink>
+//               </NavItem>
+//             </Nav>
+//           </Collapse>
+//         </Navbar>
+//       </div>
+//     )
+//   }
+// }
+
+import { slide as Menu } from 'react-burger-menu'
 
 export default class Navigation extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.toggleNavbar = this.toggleNavbar.bind(this)
-    this.state = {
-      collapsed: true
-    }
+  showSettings (event) {
+    event.preventDefault()
   }
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    })
-  }
   render () {
     return (
-      <div>
-        <Navbar color='faded' light>
-          <NavbarBrand href='/' className='mr-auto'>Logo</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className='mr-2' />
-          <Collapse isOpen={!this.state.collapsed} navbar>
-            <Nav navbar>
-              <NavItem>
-                <NavLink href='/home'>Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href='/dashboard'>Dashboard</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href='/account'>Account</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      <Menu right>
+        <a id='home' className='menu-item' href='/'>Home</a>
+        <a id='dashboard' className='menu-item' href='/dashboard'>Dashboard</a>
+        <a id='account' className='menu-item' href='/account'>Account</a>
+      </Menu>
     )
   }
 }
