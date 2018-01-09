afdddec6d965f8628ee8cfc8b6bfe737dfdb4ce2
Carmen <carmenhchung@gmail.com>
2018-01-09 18:38:06 +1100

    attempt to add iphone image to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,27 +3,17 @@ import logo from './logo.svg'
 import Checkout from './Checkout'
 import './App.css'
 import Navigation from './components/navbar'
-// import jumbotronInstance from './pages/HomePage'
-import { Jumbotron, Button } from 'react-bootstrap'
+import Homelanding from './pages/HomePage'
 
 // allow for env files
 require('dotenv').config()
-        // <jumbotronInstance />
+
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <Navigation />
-        <Jumbotron>
-          <h1>Hello, world!</h1>
-          <p>
-      		This is a simple hero unit, a simple jumbotron-style component for calling
-      		extra attention to featured content or information.
-      		</p>
-          <p>
-            <Button bsStyle='primary'>Learn more</Button>
-          </p>
-        </Jumbotron>
+        <Homelanding />
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <h1 className='App-title'>Welcome to React</h1>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import Jumbotron from 'react-bootstrap/lib/Jumbotron'
-import Button from 'react-bootstrap/lib/Button'
+import { Jumbotron, Button, Grid, Row, Col } from 'react-bootstrap'
 
-const jumbotronInstance = () => (
+const Homelanding = () => (
   <Jumbotron>
-    <h1>Hello, world!</h1>
-    <p>
-		This is a simple hero unit, a simple jumbotron-style component for calling
-		extra attention to featured content or information.
-		</p>
-    <p>
-      <Button bsStyle='primary'>Learn more</Button>
-    </p>
+    <Grid>
+      <Row className='show-grid'>
+        <Col xs={8} md={8}>
+          <br />
+          <h1>Need money?</h1>
+          <p>
+          Turn your invoices into cash today.
+          </p>
+          <Button>Sign Up</Button>
+        </Col>
+        <Col xs={4} md={4}>
+          <img src='../images/iphone.jpg' />
+        </Col>
+      </Row>
+    </Grid>
+
   </Jumbotron>
 )
-export default jumbotronInstance
+export default Homelanding
