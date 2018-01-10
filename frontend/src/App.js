4ea78ecb4ba61c2d1507664e1d45bfef6602aee4
Jon Ablondi <jonnyablondi@hotmail.com>
2018-01-10 13:08:35 +1100

    api connected to homepage, profiles and invoices now coming through on homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,44 +5,49 @@ import './App.css'
 import ProfileList from './components/ProfileList'
 import Navigation from './components/navbar'
 import Homelanding from './pages/HomePage'
+import * as profileAPI from './api/profiles'
 
 // allow for env files
 require('dotenv').config()
 
 class App extends Component {
-  state = { profiles: null}
+  state = { profiles: null }
 
   componentDidMount(){
-    this.setState({
-      profiles: [
-        {
-          _id: "5a5316b00dbd7e37f7f32723",
-          email: "jo@hotmail.com",
-          password: "12345",
-          factory_name: "Jo Ablo",
-          address: "123 Fake St",
-          hkid: "N-1191938",
-          incorporationCertificate: "bbbb",
-          paymentMethod: "ccccc",
-          invoices: [
-            {
-            _id: "5a53199665da64386f09f6ba",
-            invoiceNumber: "MKT-001-28t",
-            amount: 2553.5,
-            offerAmount: 2298.15,
-            dueDate: "2018-05-01T00:00:00.000Z",
-            expiryDate: "2018-02-01T00:00:00.000Z",
-            status: "Pending",
-            customerCompanyName: "Walmart",
-            customerFirstname: "Mary",
-            customerSurname: "Jones",
-            salePurchaseAgreement: "",
-            invoiceUpload: ""
-            }
-          ]
-        }
-      ]
-    });
+    profileAPI.all()
+    .then(profiles => {
+      this.setState({ profiles })
+    })
+    // this.setState({
+    //   profiles: [
+    //     {
+    //       _id: "5a5316b00dbd7e37f7f32723",
+    //       email: "jo@hotmail.com",
+    //       password: "12345",
+    //       factoryName: "Joe Abloe",
+    //       address: "123 Fakeee St",
+    //       hkid: "N-1191938",
+    //       incorporationCertificate: "bbbb",
+    //       paymentMethod: "cceeeccc",
+    //       invoices: [
+    //         {
+    //         _id: "5a53199665da64386f09f6ba",
+    //         invoiceNumber: "MKT-001-28t",
+    //         amount: 2553.5,
+    //         offerAmount: 2298.15,
+    //         dueDate: "2018-05-01T00:00:00.000Z",
+    //         expiryDate: "2018-02-01T00:00:00.000Z",
+    //         status: "Pending",
+    //         customerCompanyName: "Walmart",
+    //         customerFirstname: "Mary",
+    //         customerSurname: "Jones",
+    //         salePurchaseAgreement: "",
+    //         invoiceUpload: ""
+    //         }
+    //       ]
+    //     }
+    //   ]
+    // });
   }
 
   render () {
