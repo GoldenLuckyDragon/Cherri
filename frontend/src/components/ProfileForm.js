933f6dc9e086c2c32be899c85a4203ddbde98d6a
Jon Ablondi <jonnyablondi@hotmail.com>
2018-01-11 14:34:05 +1100

    fixed issue with redirect on profile creation

--- frontend/src/components/ProfileForm.js
+++ frontend/src/components/ProfileForm.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Redirect} from 'react-router-dom'
 
-export default function ProfileForm ({onSubmit}) {
-  function handleFormSubmission (event) {
+class ProfileForm extends React.Component {
+  state = { redirect: false}
+
+  handleFormSubmission = (event) => {
     event.preventDefault()
-    // the opposite, but not best practice
-    // event.stopPropagation();
     const { elements } = event.target
     const email = elements['email'].value
     const password = elements['password'].value
@@ -13,55 +14,64 @@ export default function ProfileForm ({onSubmit}) {
     const hkid = elements['hkid'].value
     const incorporationCertificate = elements['incorporationCertificate'].value
     const paymentMethod = elements['paymentMethod'].value
-    onSubmit({email, password, factoryName, address, hkid, incorporationCertificate, paymentMethod})
+    this.props.onSubmit({email, password, factoryName, address, hkid, incorporationCertificate, paymentMethod})
+    this.setState({ redirect: true })
   }
 
-  return (
-    <form onSubmit={handleFormSubmission} >
-      &nbsp;
-      <label>
-        Email:
-        &nbsp;
-        <input type='text' name='email' />
-      </label>
+  render() {
+    const {redirect} = this.state
+    return (
+      <div>
+        { redirect && <Redirect to="/profiles" />}
+        <form onSubmit={this.handleFormSubmission} >
+          &nbsp;
+          <label>
+            Email:
+            &nbsp;
+            <input type='text' name='email' />
+          </label>
 
-      <label>
-        Password:
-        &nbsp;
-        <input type='text' name='password' />
-      </label>
+          <label>
+            Password:
+            &nbsp;
+            <input type='text' name='password' />
+          </label>
 
-      <label>
-        Factory Name:
-        &nbsp;
-        <input type='text' name='factoryName' />
-      </label>
+          <label>
+            Factory Name:
+            &nbsp;
+            <input type='text' name='factoryName' />
+          </label>
 
-      <label>
-        Address:
-        &nbsp;
-        <input type='text' name='address' />
-      </label>
+          <label>
+            Address:
+            &nbsp;
+            <input type='text' name='address' />
+          </label>
 
-      <label>
-        HK ID:
-        &nbsp;
-        <input type='text' name='hkid' />
-      </label>
+          <label>
+            HK ID:
+            &nbsp;
+            <input type='text' name='hkid' />
+          </label>
 
-      <label>
-        Incorporation Certificate:
-        &nbsp;
-        <input type='text' name='incorporationCertificate' />
-      </label>
+          <label>
+            Incorporation Certificate:
+            &nbsp;
+            <input type='text' name='incorporationCertificate' />
+          </label>
 
-      <label>
-        Payment Method:
-        &nbsp;
-        <input type='text' name='paymentMethod' />
-      </label>
+          <label>
+            Payment Method:
+            &nbsp;
+            <input type='text' name='paymentMethod' />
+          </label>
 
-      <button type='submit'>Create Profile</button>
-    </form>
-  )
+          <button type='submit'>Create Profile</button>
+        </form>
+      </div>
+    )
+  }
 }
+
+export default ProfileForm
