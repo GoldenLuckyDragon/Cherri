2207763d7b5056a7eddb9957b7d6adba4e4bda31
J1marotta <jim_is55@hotmail.com>
2018-01-15 13:35:15 +1100

    Hard code server address

--- frontend/src/constants/server.js
+++ frontend/src/constants/server.js
@@ -1,9 +1,9 @@
 // set the url if we are in production environment
-const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
+// const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
+//
+// // update the top one to zeit directory.
+// ? 'http://cherri.now.sh'
+// // or if in dev env use our api local host
+// : 'http://localhost:8080'
 
-// update the top one to zeit directory.
-? 'http://cherri.now.sh'
-// or if in dev env use our api local host
-: 'http://localhost:8080'
-
-export default PAYMENT_SERVER_URL
+export default PAYMENT_SERVER_URL = 'https://cherri.now.sh'
