e52edbea946c63c1c456c343c479380afd593a0b
J1marotta <jim_is55@hotmail.com>
2018-01-15 13:02:40 +1100

    Fix Navbar

--- backend/models/base.js
+++ backend/models/base.js
@@ -1,9 +1,18 @@
+// set up constant mongo url
+// const MONGO_URI = process.env.NODE_ENV === 'production'
+// ? process.env.MONGO_PROD_URI
+// : process.env.MONGO_DEV_URI
+
+const MONGO_URI = 'mongodb://cherri:finance@ds255797.mlab.com:55797/cherrifinance'
+
 // Include mongoose and set up our connection
 const mongoose = require('mongoose')
 mongoose.Promise = Promise
 const db = mongoose.connection
 
 db.on('open', () => { console.log('Successful connection to MongoDB') })
-mongoose.connect('mongodb://localhost/finalProfiles', { useMongoClient: true })
+
+// connect to our online Mongo
+mongoose.connect(MONGO_URI, { useMongoClient: true })
 
 module.exports = mongoose
--- frontend/src/constants/server.js
+++ frontend/src/constants/server.js
@@ -1,7 +1,9 @@
 // set the url if we are in production environment
-
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-? 'http://myapidomain.com'
+
+// update the top one to zeit directory.
+? 'http://cherri.now.sh'
+// or if in dev env use our api local host
 : 'http://localhost:8080'
 
 export default PAYMENT_SERVER_URL
