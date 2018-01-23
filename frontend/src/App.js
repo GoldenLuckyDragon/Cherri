841a2df267b68891cb1f388da988a57994fc06df
Jon Ablondi <jonnyablondi@hotmail.com>
2018-01-23 14:31:32 +1100

    uploadic working in user journey and when logged out homepage still functioning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,7 @@ import RegisterForm from './components/RegisterForm'
 import SignInForm from './components/SignInForm'
 import SignOutForm from './components/SignOutForm'
 import UploadHkid from './components/UploadHkid'
+import UploadIc from './components/UploadIc'
 import * as auth from './api/signin'
 import * as userAPI from './api/user'
 import Navigation from './components/navbar'
@@ -204,6 +205,14 @@ class App extends Component {
               () => {
                 if (auth.isSignedIn() && users) {
                   return <UploadHkid users={users}/>
+                } else {
+                  return null
+                }
+              }}/>
+          <Route path='/uploadIc' render={
+              () => {
+                if (auth.isSignedIn() && users) {
+                  return <UploadIc users={users}/>
                 } else {
                   return null
                 }
