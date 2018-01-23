dbaf1bdf314e0caccfeb38973838bc75d029250e
Carmen <carmenhchung@gmail.com>
2018-01-23 12:18:33 +1100

    add display of cloudinary images to account page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import Invoice from './Invoice'
 import {Link} from 'react-router-dom'
-// import { Jumbotron } from 'react-bootstrap'
-// import Logo from '../components/Logo'
-// import Navigation from '../components/navbar'
 import Checkout from '../components/Checkout'
+import {Image, CloudinaryContext} from 'cloudinary-react'
+var cloudinary = require('cloudinary')
 
 export default function Profile ({
   _id,
@@ -24,41 +23,12 @@ export default function Profile ({
       <br />
       <span>Address: {henry.address}</span>
       <br />
-      <span>HKID: {henry.hkid}</span>
+      <span>HKID:</span>
+      <CloudinaryContext cloudName='cherri'>
+        <Image publicId='companydocs/5a66880978baec24c24d0940_hkid.png' />
+      </CloudinaryContext>
       <br />
       <br />
     </div>
   )
 }
-
-// Invoices: {
-  // barry ? (
-  //   barry.map(invoice => {
-  //     return (
-  //       <div>
-  //         <span>Invoice Number:{invoice.invoiceNumber} </span>
-  //         <br />
-  //         <span>Invoice Amount:{invoice.amount} </span>
-  //         <br />
-  //         <span>Invoice Currency:{invoice.currency} </span>
-  //         <br />
-  //         <span>Offer Amount:{invoice.offerAmount} </span>
-  //         <br />
-  //         <span>Due Date:{invoice.dueDate} </span>
-  //         <br />
-  //         <span>Expiry Date:{invoice.expiryDate} </span>
-  //         <br />
-  //         <span>Status:{invoice.status} </span>
-  //       &nbsp;
-  //
-  //       <Checkout
-  //         name={` Pay ${invoice.customerCompanyName}`}
-  //         description={` Invoice :${invoice.invoiceNumber}`}
-  //         amount={invoice.offerAmount}
-  //         // payee={stripeId}
-  //       />
-  //       </div>
-  //     )
-  //   })
-  //   // or show no tokens
-  // ) : ('You have no submitted invoices available.')
